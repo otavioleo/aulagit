55958b2553319af0e3ca59e278c0b5b8495ba0cb
Otavio Ap. Leonardo <otavio.leonardo@campinas.sp.gov.br>
2016-02-04 16:38:22 -0200

    Modificando arquivos   04/02/2016 - 16:40

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
 echo "Olá mundo!<br>";
 echo "Esquecide colocar a mensagem no primeiro commit";
+echo "Terceira modificação index";
 
--- teste.php
+++ teste.php
@@ -1,5 +1,7 @@
 <?php
 echo "Criando arquivo teste.php";
+echo "Primeira modificação teste";
+
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
